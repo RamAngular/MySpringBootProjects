bbb734c6e6c9ad570cb6f72622b40268dfb404e7
Rami Reddy Vakamalla <31163150+RamiReddyFullstackDeveloper@users.noreply.github.com>
2023-04-13 10:39:03 +0530

    email notification test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! maven integration with Jenkins" );
+        System.out.println( "Hello World! maven integration with Jenkins" )
     }
 }
